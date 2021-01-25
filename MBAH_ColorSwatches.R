d614341d4b3854ae693efcf56b8a5b8ece2d8e54
MZaharieva <m.st.zaharieva@gmail.com>
2021-01-25 13:06:57 +0100

    i & i+2 color pairs
    Pair every other color.

--- MBAH_ColorSwatches.R
+++ MBAH_ColorSwatches.R
@@ -18,10 +18,13 @@ plot(stim_colors)
 
 # Save the resulting matrix into a dataframe. Compute the difference in hue between adjacent values.
 stim_colors_hex <- as.data.frame(stim_colors) %>% 
-  as.data.frame(hex(polarLUV(L = stim_colors_hex$L, C = stim_colors_hex$C, H = stim_colors_hex$H), fixup = FALSE)) %>%
+  as.data.frame(hex(polarLUV(L = .$L, C = .$C, H = .$H), fixup = FALSE)) %>%
   tibble::rownames_to_column(., var = "hex_code") %>%
+  mutate(., color_index = row_number(), color_pair = color_index + 2) %>%
+  inner_join(x = ., y = ., by = c("color_index" = "color_pair")) %>%
+  select(hex_code_c1 = hex_code.x, hex_code_c2 = hex_code.y) %>%
   # Append grey value (same luminance) used for background stimulus display color:#b0aca9  
-  dplyr::bind_rows(tibble(hex_code = "#b0aca9")) 
+  dplyr::bind_rows(tibble(hex_code_c1 = "#b0aca9")) 
 
-# Export as .csv.  
-write.csv2(stim_colors_hex, file = 'MBAH_colors.csv', sep = ",", dec = ".", col.names = TRUE, row.names = FALSE)
+# EXPORT .csv. ----
+write.csv2(stim_colors_hex, file = 'MBAH_colors.csv', sep = ",", dec = ".", col.names = TRUE, row.names = TRUE)
